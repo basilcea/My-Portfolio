b1265c9396a4520d7aeace454a3a392c655c55b3
basilcea <ogbonnabasil3@gmail.com>
2019-10-05 02:59:49 +0100

    fix bug in downloads

--- src/views/home.js
+++ src/views/home.js
@@ -5,7 +5,7 @@ import icon from "../assests/icon.png";
 import "../animate.css";
 import axios from 'axios';
 import Typed from "react-typed";
-import dotenv from  'dot-env';
+import dotenv from  'dotenv';
 import {
   FaEnvelope,
   FaMapMarkerAlt,
@@ -133,7 +133,7 @@ outline:none;
 dotenv.config()
 const Home = () => {
   const downloading = async() =>{
-    await axios.get('https://cors-wahala.herokuapp.com/https://www.googleapis.com/drive/v3/files/1275c99fPn4m7O3Mhs67LtLIkgVyED72f`
+    await axios.get('https://cors-wahala.herokuapp.com/https://www.googleapis.com/drive/v3/files/1275c99fPn4m7O3Mhs67LtLIkgVyED72f'
    , {
  headers: {
    Authorization: 'Bearer ' + process.env.Token //the token is a variable which holds the token
